a2b2f28af7baa85047aa0476cd16b4aa9e6af0cd
Shubha <shubha.acharya@cognizant.com>
2021-03-18 19:26:49 +0530

    Update License, Author

--- src/main/java/com/cognizant/dashboard/collectors/github/config/GlobalConfiguration.java
+++ src/main/java/com/cognizant/dashboard/collectors/github/config/GlobalConfiguration.java
@@ -1,3 +1,19 @@
+/*
+ * © [2021] Cognizant. All rights reserved.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
 package com.cognizant.dashboard.collectors.github.config;
 
 import com.cognizant.dashboard.collectors.github.client.GitHubClient;
@@ -15,18 +31,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.concurrent.TimeUnit;
-
+/**
+ * GlobalConfiguration -
+ * @author Cognizant
+ */
 
 @Configuration
 @Slf4j
 public class GlobalConfiguration {
 
-//    private static final String AUTHORIZATION = "Authorization";
     @Value("${github.url}")
     private String githubUrl;
-//    @Value("Bearer ${github.token}")
-//    private String githubToken;
-
 
     @Bean
     public GitHubClient gitHubClient() {
--- src/main/java/com/cognizant/dashboard/collectors/github/db/impl/CustomRepositoryImpl.java
+++ src/main/java/com/cognizant/dashboard/collectors/github/db/impl/CustomRepositoryImpl.java
@@ -1,3 +1,19 @@
+/*
+ * © [2021] Cognizant. All rights reserved.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
 package com.cognizant.dashboard.collectors.github.db.impl;
 
 import com.cognizant.dashboard.collectors.github.beans.repos.GitHubRepos;
@@ -10,6 +26,10 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+/**
+ * CustomRepositoryImpl
+ * @author Cognizant
+ */
 
 @Repository
 public class CustomRepositoryImpl implements CustomRepository {
